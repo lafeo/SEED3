100845dd87347c70f99f4b381d1ff3efbf3a886b
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-04-11 04:01:06 +0530

    Added star API

--- frontend/src/components/pages/SeedDetails.js
+++ frontend/src/components/pages/SeedDetails.js
@@ -3,17 +3,59 @@ import axios from "axios";
 import { BACKEND_URL } from "../../constants";
 import styled from "styled-components";
 import "./Details.scss";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {faStar as outlined} from '@fortawesome/free-regular-svg-icons';
 
 export default function SeedDetailsComponent(props) {
   const [allCrawlers, setAllCrawlers] = useState([]);
   const [mainSeed, setMainSeed] = useState(props.location.state.seed);
-  const [showData, setShowData] = useState(false);
-  const [showSeed, setShowSeed] = useState(false);
+  const [starCounter,setStarCounter] = useState(props.location.state.seed.stars);
+  const [seedStarred,setSeedStarred] = useState(false);
+  const [body,setBody] = useState('');
+  const TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imt1bmFsYmhhdGlhQGdtYWlsLmNvbSIsImZpcnN0TmFtZSI6Ikt1bmFsIiwibGFzdG5hbWUiOiJCaGF0aWEiLCJ1c2VybmFtZSI6IktCaGF0aWEiLCJpZCI6IjYwNzA1MGYyN2Y5ZmI1MDNlNzVkNTA4MCIsIm51bWJlck9mU2VlZHNXcml0dGVuIjo1LCJudW1iZXJPZkNyYXdsc1dyaXR0ZW4iOjEsImlhdCI6MTYxODA5MTU3NiwiZXhwIjoxNjE4MTAyMzc2fQ.RAw9X5ovxa61dhM-nnt0ztaB1ElpQ-a2dOME9rJrxlg';
   function renderCrawlers() {
     return allCrawlers.map((crawler) => <h1>crawler.authorID</h1>);
   }
+  function changeSeedStarred(){
+    axios.put(`${BACKEND_URL}writing-routes/update-writing-stars/${mainSeed._id}`,{
+      newNumberOfStars:(starCounter + (!seedStarred ? 1:-1))
+    },{
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization' : `Bearer ${TOKEN}`
+      }
+    }).then(response=>{
+      response=response.data;
+      if (response.success){
+        console.log("Stars Updated!");
+        setStarCounter(starCounter + (!seedStarred?1:-1))
+        console.log(response);
+        setSeedStarred(!seedStarred);
+      }else{
+        console.log("Could not update stars!!");
 
-  useEffect(() => {
+      }
+
+    }).catch(err=>{
+      console.log("Error updating number of stars!!");
+      console.log(err);
+    })
+
+  }
+
+  useEffect(async() => {
+    await axios.get(`${BACKEND_URL}writing-routes/get-seed-body-and-stars/${props.location.state.seed._id}`).then(response=>{
+      if (response.data.success){
+        setBody(response.data.body);
+        setStarCounter(response.data.stars);
+      }
+    }).catch(err=>{
+      console.log("Error!");
+      console.log(err);
+    })
+
+
     axios
       .get(
         `${BACKEND_URL}writing-routes/get-crawlers-for-seed/${props.location.state.seed._id}/`
@@ -25,7 +67,6 @@ export default function SeedDetailsComponent(props) {
           console.log(mainSeed);
 
           setAllCrawlers(allCrawlers.data.allCrawlers);
-          setShowData(true);
         } else {
           console.log("Could not fetch all crawlers!");
         }
@@ -42,11 +83,26 @@ export default function SeedDetailsComponent(props) {
   return (
     <Full>
       <SeedContainer>
-        <SeedWrapper>
+       <SeedWrapper>
+          <SeedTitle>{mainSeed.title}
+            <StarsArea>
+              {starCounter}
+            </StarsArea>
+          </SeedTitle>
+
+
+          <AuthorName>  By {mainSeed.userDetails.username}
+            <StarsArea>
+              <FontAwesomeIcon onClick={changeSeedStarred} icon={seedStarred ? faStar : outlined}/>
+            </StarsArea>
+
+          </AuthorName>
           <Seed mainSeed={mainSeed}>
-            <div dangerouslySetInnerHTML={{ __html: mainSeed.body }} />
+            <h3 dangerouslySetInnerHTML={{ __html: body}} />
+
           </Seed>
         </SeedWrapper>
+
         <CrawlerContainer>
           <CrawlerWrapper>
             {allCrawlers.map((crawler) => (
@@ -63,6 +119,17 @@ export default function SeedDetailsComponent(props) {
   );
 }
 
+const SeedTitle = styled.div`
+  font-size:2.3rem;
+  text-align:center;
+`;
+
+const StarsArea = styled.div`
+  float:right;
+  font-size:1.6rem;
+`
+
+
 const Full = styled.div`
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("/images/wallpaper.jpg");
@@ -71,6 +138,12 @@ const Full = styled.div`
   overflow: hidden;
   height: 87vh;
 `;
+const AuthorName = styled.div`
+  text-align: center;
+  font-size:1.1rem;
+  margin:0.3rem;
+`;
+
 
 const SeedContainer = styled.div`
   display: grid;
@@ -82,8 +155,8 @@ const SeedContainer = styled.div`
 const SeedWrapper = styled.div`
   color: white;
   /* margin: 3rem; */
-  height: 60vh;
-  max-width: 50vw;
+  height: 80vh;
+  max-width: 70vw;
   backdrop-filter: blur(5px);
   background-color: rgba(255, 255, 255, 0.1);
   border-radius: 20px;
@@ -94,7 +167,7 @@ const SeedWrapper = styled.div`
 `;
 
 const Seed = styled.div`
-  font-size: 2rem;
+  font-size: 1.1rem;
   color: white;
   display: flex;
   flex-direction: column;
