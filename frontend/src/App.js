b0e5ea8554d2aaf026b3eca00b2bf27e68a73f2e
SpaceFace02 <73184157+SpaceFace02@users.noreply.github.com>
2021-04-10 13:06:55 +0530

    tinymce problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Services from "./components/pages/Services";
 import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
+import AddCrawler from "./components/AddCrawler";
+import AddNewSeed from "./components/AddNewSeed";
 
 function App() {
   return (
@@ -13,10 +15,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" exact component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/services" component={Services} />
           <Route exact path="/log-in" component={Login} />
           <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/add-new-seed" component={AddNewSeed} />
+          <Route exact path="/add-new-crawler" component={AddCrawler} />
         </Switch>
       </Router>
     </>
