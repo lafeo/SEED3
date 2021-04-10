43b32eceb519c1d85cdec06a7599961ac05de602
SpaceFace02 <73184157+SpaceFace02@users.noreply.github.com>
2021-04-10 08:59:13 +0530

    Map function sorted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Login from './components/pages/Login';
-import SignUp from './components/pages/SignUp';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path='/' exact component={Home} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/log-in' component={Login} />
-          <Route exact path='/sign-up' component={SignUp} />
+          <Route exact path="/" exact component={Home} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/log-in" component={Login} />
+          <Route exact path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,67 +1,79 @@
-import React,{useEffect,useState} from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React, { useEffect, useState } from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
 import axios from "axios";
 
-import {BACKEND_URL} from "../constants";
-
+import { BACKEND_URL } from "../constants";
 
 function Cards() {
-  const [allSeeds,setAllSeeds] = useState([]);
-  const [showSeeds,setShowSeeds] = useState(false);
-  useEffect(()=>{
-    axios.get('http://localhost:8010/writing-routes/get-all-seeds/').then(allSeeds=>{
-      console.log(allSeeds.data);
-      setAllSeeds(allSeeds.data.allSeeds);
-      setShowSeeds(true);
-      console.log(BACKEND_URL);
-    }).catch(err=>{
-      console.log(err);
-    })
+  const [allSeeds, setAllSeeds] = useState([]);
+  const [showSeeds, setShowSeeds] = useState(false);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8010/writing-routes/get-all-seeds/")
+      .then((allSeeds) => {
+        console.log(allSeeds.data);
+        setAllSeeds(allSeeds.data.allSeeds);
+        setShowSeeds(true);
+        console.log(BACKEND_URL);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  function Shuffle(array) {
+    let ctr = array.length,
+      temp,
+      index;
 
-  },[]);
-
-  function renderFirstFive(){
-    return allSeeds.splice(5).map(seed=>(
-          <CardItem
-              src={BACKEND_URL+seed.imageURL}
-              text={seed.title}
-              label={seed.author}
-              path='/services'
-          />
-    ))
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = array[ctr];
+      array[ctr] = array[index];
+      array[index] = temp;
+    }
+    return array;
   }
 
-  return  (
-      <div className='cards'>
-    <h1>Check out these EPIC Articles written by some top authors!</h1>
-        {showSeeds ? ( <div className='cards__container'>
-          <div className='cards__wrapper'>
-            <ul className='cards__items'>
-              <CardItem
-                  src={BACKEND_URL + allSeeds[0].imageURL}
-                  text={allSeeds[0].title}
-                  label='Adventure'
-                  path='/services'
-              />
-              <CardItem
-                  src={BACKEND_URL + allSeeds[1].imageURL}
-                  text={allSeeds[1].title}
-                  label='Luxury'
-                  path='/services'
-              />
-              <CardItem
-                  src={BACKEND_URL+allSeeds[2].imageURL}
-                  text={allSeeds[2].title}
-                  label={allSeeds[2].author}
-                  path='/services'
-              />
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Articles written by some top authors!</h1>
+      {showSeeds ? (
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              {Shuffle(allSeeds)
+                .splice(0, 4)
+                .map((seed) => (
+                  <CardItem seed={seed} />
+                ))}
             </ul>
           </div>
-        </div>) : null}
-  </div>)
-
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Cards;
+
+/* <CardItem
+                src={BACKEND_URL + allSeeds[0].imageURL}
+                text={allSeeds[0].title}
+                label="Adventure"
+                path="/services"
+              />
+              <CardItem
+                src={BACKEND_URL + allSeeds[1].imageURL}
+                text={allSeeds[1].title}
+                label="Luxury"
+                path="/services"
+              />
+              <CardItem
+                src={BACKEND_URL + allSeeds[2].imageURL}
+                text={allSeeds[2].title}
+                label={allSeeds[2].author}
+                path="/services"
+              /> */
