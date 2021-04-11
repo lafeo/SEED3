1a7fe73c247d93f1e9841cce88e6e611aec3a0a9
SpaceFace02 <73184157+SpaceFace02@users.noreply.github.com>
2021-04-11 13:01:44 +0530

    Scale Prop

--- frontend/src/components/card2.js
+++ frontend/src/components/card2.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react'
-import axiom from '../axiom.jpg';
+import React, { Component } from "react";
+import axiom from "../axiom.jpg";
+import styled from "styled-components";
 
-import './pages/login.css';
-import './card2.css';
+import "./pages/login.css";
+import "./card2.css";
 
-const Card = props => {
-    return(
-        <>
-    <div className="card-body">
+const Card = (props) => {
+  return (
+    <>
+      <CardBody className="card-body">
+        <div className="card text-center">
+          <div className="overflow">
+            <img
+              className={"portal-cards-image"}
+              src={props.imagePath}
+              alt='professionals"'
+            ></img>
+          </div>
+          <div className="text-dark">
+            <h4 className="card-title">{props.title}</h4>
 
-    <div className="card text-center">
-      <div className="overflow">
-        <img className={'portal-cards-image'} src={props.imagePath}></img>
-      </div>
-      <div className="text-dark">
-        <h4 className="card-title">{props.title}</h4>
-
-        {/*<a href="#" className="btn btn-outline-success">Go anywhere</a>*/}
-      </div>
-    </div>
-    </div>
+            {/* <a href="#" className="btn btn-outline-success">
+              Go anywhere
+            </a> */}
+          </div>
+        </div>
+      </CardBody>
     </>
-    );
-}
+  );
+};
 export default Card;
 
+const CardBody = styled.div`
+  cursor: pointer;
+  transition: 0.4s ease-out all;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
